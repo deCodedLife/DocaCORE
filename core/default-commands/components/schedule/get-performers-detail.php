ee9f364f013407f39f821b9bada12bc3830024e6
Vladislav_TS <vladislav.tsyrdea@gmail.com>
2022-12-19 15:55:07 +0300

    schedule hook add

--- core/default-commands/components/schedule/get-performers-detail.php
+++ core/default-commands/components/schedule/get-performers-detail.php
@@ -8,5 +8,13 @@
 $performersRows = $API->DB->from( $requestData->performers_table )
     ->where( $performersFilter );
 
+/**
+ * @hook
+ * Отработка стандартного get запроса
+ */
+if ( file_exists( $public_customCommandDirPath . "/hooks/get-performers.php" ) )
+    require( $public_customCommandDirPath . "/hooks/get-performers.php" );
+
+
 foreach ( $performersRows as $performersRow )
     $performersDetail[ $performersRow[ "id" ] ] = $performersRow[ $requestData->performers_title ];
