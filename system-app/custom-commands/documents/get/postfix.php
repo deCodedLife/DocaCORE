17ac447ee1dfcf34b10bf8e19df35223496e4b86
CodedLife <dcodedlife@gmail.com>
2023-12-28 10:42:04 +0300

    [ bugfix ] Add initials to schedule

--- system-app/custom-commands/documents/get/postfix.php
+++ system-app/custom-commands/documents/get/postfix.php
@@ -63,5 +63,3 @@ if ( $requestData->id || $requestData->article ) {
  */
 if ( file_exists( $public_customCommandDirPath . "/hooks/result.php" ) )
     require( $public_customCommandDirPath . "/hooks/result.php" );
-
-$API->returnResponse( $requestData );
